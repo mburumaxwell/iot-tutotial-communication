9cb16eab713d99f593091ab8f8c21bf4c9514c6e
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 11:21:21 +0300

    added DNS client (needed to resolve host names to IP addresses)

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -21,6 +21,7 @@
           #define RTE_Network_IPv4                /* Network IPv4 Stack */
           #define RTE_Network_IPv6                /* Network IPv6 Stack */
           #define RTE_Network_Release             /* Network Release Version */
+#define RTE_Network_DNS_Client          /* Network DNS Client */
 #define RTE_Network_Interface_PPP       /* Network Interface PPP */
 #define RTE_Network_Socket_BSD          /* Network Socket BSD */
 #define RTE_Network_Socket_TCP          /* Network Socket TCP */
