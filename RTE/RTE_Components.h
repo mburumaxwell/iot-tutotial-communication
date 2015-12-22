e6c6358816064fb2bd1d2a26c436b15b43475d44
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 10:46:21 +0300

    added the network stack

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -17,5 +17,13 @@
         #define RTE_Drivers_USART1              /* Driver USART1 */
         #define RTE_Drivers_USART2              /* Driver USART2 */
         #define RTE_Drivers_USART3              /* Driver USART3 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+          #define RTE_Network_IPv6                /* Network IPv6 Stack */
+          #define RTE_Network_Release             /* Network Release Version */
+#define RTE_Network_Interface_PPP       /* Network Interface PPP */
+#define RTE_Network_Socket_BSD          /* Network Socket BSD */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
 
 #endif /* RTE_COMPONENTS_H */
