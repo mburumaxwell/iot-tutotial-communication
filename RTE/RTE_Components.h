b6b536238e1ac46225cb6de4bb466171d6e741e7
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 11:43:12 +0300

    added printf via SWO

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -12,6 +12,8 @@
 
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+        #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
 #define RTE_DEVICE_STARTUP_LPC43XX      /* Device Startup for NXP43XX */
 #define RTE_Drivers_USART0              /* Driver USART0 */
         #define RTE_Drivers_USART1              /* Driver USART1 */
