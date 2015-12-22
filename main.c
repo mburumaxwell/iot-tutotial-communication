341a70748edd57bac213944d3de54bd3a1d8801e
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 10:56:54 +0300

    initialize the network stack

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "LPC43xx.h"                    // Device header
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
+#include "rl_net.h"                     // Keil.MDK-Pro::Network:CORE
 
 int main(void) {
-  osKernelInitialize();                 // Initialize CMSIS-RTOS
-  SystemCoreClockUpdate();				// Update the clock variable incase we need it
+	osKernelInitialize();       // Initialize CMSIS-RTOS
+	SystemCoreClockUpdate();    // Update the clock variable in case we need it
 	
-  osKernelStart();                      // Start thread execution 
+	netInitialize();            // Initialize the network stack
+	osDelay(500);               // Allow the network stack to be initialized
+	
+
+	osKernelStart();            // Start thread execution 
 }
