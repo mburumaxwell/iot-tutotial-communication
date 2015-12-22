194079e20cc1f74791bf302bb7d9c9b5af16ca85
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 10:33:07 +0300

    created project for LPC4337 Cortex M4 core

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'iot-tutotial-communication' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_STARTUP_LPC43XX      /* Device Startup for NXP43XX */
+
+#endif /* RTE_COMPONENTS_H */
