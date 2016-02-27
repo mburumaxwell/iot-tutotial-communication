070ad960f92bcc2dfb11f84e5b51e40f16f64994
Maxwell Weru <mburumaxwell@gmail.com>
2016-02-27 21:32:12 +0300

    upgrade LPC4300 pack to 2.6

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -10,6 +10,12 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC43xx.h"
+
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
